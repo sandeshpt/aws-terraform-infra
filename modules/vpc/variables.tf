5cc7eabfc61c4854effc2b5f67736dae846be616
Sandesh Prabhakar T <sandeshpt94@gmail.com>
2026-09-12 08:52:45 +0530

    Add modules/vpc/variables.tf

---
+++ modules/vpc/variables.tf
@@ -0,0 +1,20 @@
+variable "name" {
+  type = string
+}
+
+variable "vpc_cidr" {
+  type = string
+}
+
+variable "public_subnet_cidr" {
+  type = string
+}
+
+variable "availability_zone" {
+  type = string
+}
+
+variable "tags" {
+  type    = map(string)
+  default = {}
+}
